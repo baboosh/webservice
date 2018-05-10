94bdabf76e40fb2286156f05d7b5162d0912958e
masterprogrammer <bethkemax>
2018-05-10 10:13:56 -0500

    issuebot code for heruko

---
+++ webservice/__main__.py
@@ -0,0 +1,45 @@
+import os
+import aiohttp
+
+from aiohttp import web
+
+from gidgethub import routing, sansio
+from gidgethub import aiohttp as gh_aiohttp
+
+router = routing.Router()
+
+
+@router.register("issues", action="opened")  # When an issue is opened
+async def issue_opened_event(event, gh, *args, **kwargs):
+    """
+    Whenever an issue is opened, greet the author and say thanks.
+    """
+    url = event.data["issue"]["comments_url"]  # Get the url to comment from the event data
+    author = event.data["issue"]["user"]["login"]  # Get the author that issued from the event data
+
+    message = f"Thanks for the report @{author}! I will look into it ASAP! (I'm a bot)."  # Constructed Message
+    await gh.post(url, data={"body": message})  # Post the comment with the message.
+
+
+async def main(request):
+    body = await request.read()
+
+    secret = os.environ.get("GH_SECRET")
+    oauth_token = os.environ.get("GH_AUTH")
+
+    event = sansio.Event.from_http(request.headers, body, secret=secret)
+    async with aiohttp.ClientSession() as session:
+        gh = gh_aiohttp.GitHubAPI(session, "baboosh",  # Replace the second parameter with your own username
+                                  oauth_token=oauth_token)
+        await router.dispatch(event, gh)
+    return web.Response(status=200)
+
+
+if __name__ == "__main__":
+    app = web.Application()
+    app.router.add_post("/", main)
+    port = os.environ.get("PORT")
+    if port is not None:
+        port = int(port)
+
+    web.run_app(app, port=port)
